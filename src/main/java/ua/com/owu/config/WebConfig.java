b971faf97bbe0500f72dd0119e4a558f7c05f003
DartNazariaN <nestoraknazik@gmail.com>
2018-09-06 23:52:50 +0300

    create class "WebConfig" in package "config"

---
+++ src/main/java/ua/com/owu/config/WebConfig.java
@@ -0,0 +1,32 @@
+package ua.com.owu.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan("ua.com.owu")
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/css/**")
+                .addResourceLocations("/static/resources/style/");
+        registry
+                .addResourceHandler("/img/**")
+                .addResourceLocations("/static/resources/images/");
+    }
+}
